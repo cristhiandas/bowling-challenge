b3ac39a09ff35060bd9ed40637833e5ec0211846
cristhiandas <cristhiandf94@gmail.com>
2017-11-26 16:33:22 +0000

    created test for the first and second shot

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -6,7 +6,34 @@ describe('bowling', function () {
     bowlingscore = new BowlingScore()
   })
 
-  it('Have frames', function () {
-    expect(bowlingscore.frames).toEqual(10)
+  describe('When initializing', function () {
+    it('Have frames', function () {
+      expect(bowlingscore.frames).toEqual(10)
+    })
+
+    it('Have have strike as false', function () {
+      expect(bowlingscore.isStrike).toEqual(false)
+    })
+
+    it('Have have spare as false', function () {
+      expect(bowlingscore.isSpare).toEqual(false)
+    })
+  })
+
+  describe('When doing a first shot', function () {
+    it('returns the score', function () {
+      expect(bowlingscore.firstshot(5)).toEqual(5)
+    })
+
+    it('Tells when there is a Strike', function () {
+      bowlingscore.firstshot(10)
+      expect(bowlingscore.isStrike).toEqual(true)
+    })
+  })
+
+  describe('When doing a second shot', function () {
+    it('returns the score', function () {
+      expect(bowlingscore.secondshot(4)).toEqual(4)
+    })
   })
 })
